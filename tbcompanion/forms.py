44bb952dba04d145a6aebd4e7652e98035f692e5
GiorginoSerbuciano <64908978+GiorginoSerbuciano@users.noreply.github.com>
2021-02-27 09:47:53 +0100

    Building password reset

--- tbcompanion/forms.py
+++ tbcompanion/forms.py
@@ -1,25 +1,25 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
-from wtforms.fields.core import BooleanField, StringField
+from wtforms.fields.core import BooleanField, SelectField, StringField
 from wtforms.fields.simple import SubmitField, PasswordField, TextAreaField, TextField
 from wtforms.validators import DataRequired, EqualTo, Length, Email, URL, ValidationError
+from wtforms.widgets.core import CheckboxInput
 from tbcompanion.models import User
 from tbcompanion import bcrypt
 
 
 class RegistrationForm(FlaskForm):
-
-	username = StringField('Username', validators = [
+	username = StringField('Username', validators=[
 		DataRequired(),
-		Length(min=5, max=20)		
+		Length(min=5, max=20)
 	])
 	email = StringField('Email', validators=[
 		DataRequired(),
-		Email()		
+		Email()
 	])
 	password = PasswordField('Password', validators=[
 		DataRequired(),
-		Length(min=8)	
+		Length(min=8)
 	])
 	confirm_password = PasswordField('Confirm Password', validators=[
 		DataRequired(),
@@ -31,46 +31,47 @@ class RegistrationForm(FlaskForm):
 		user = User.query.filter_by(username=username.data).first()
 		if user:
 			raise ValidationError('Username already exists.')
+
 	def validate_email(self, email):
 		user = User.query.filter_by(email=email.data).first()
 		if user:
-			raise ValidationError('Email is already in use.')	
+			raise ValidationError('Email is already in use.')
 
 
 class LoginForm(FlaskForm):
-	
-	#username not used for login
+	# username not used for login
 	email = StringField('Email', validators=[
 		DataRequired(),
-		Email()		
+		Email()
 	])
 	password = PasswordField('Password', validators=[
-		DataRequired()	
+		DataRequired()
 	])
 	submit = SubmitField('Log me in.')
 	remember = BooleanField('Remember me.')
 
 
 class ProjectForm(FlaskForm):
-
 	title = StringField('Title', validators=[
 		DataRequired(),
 		Length(min=2, max=80)
 	])
-	content = TextAreaField('Insert Text Here', validators=[
+	content = TextAreaField('Describe your project', validators=[
 		DataRequired()
 	])
 	github_repo = StringField('URL to GitHub Repository', validators=[
 		Length(max=120),
 		URL('https://github.com/')
 	])
+	drop_tag = ['None', 'Art', 'Social', 'Data', 'Library', 'Meta', 'Other']
+	tag = SelectField('Tag', choices=drop_tag, default=1)
 	submit = SubmitField('Release project')
 
 
 class PostForm(FlaskForm):
-	
 	title = StringField('Title', validators=[
 		DataRequired(),
+		Length(max=120)
 	])
 	content = TextAreaField('Insert Text Here', validators=[
 		DataRequired()
@@ -78,21 +79,15 @@ class PostForm(FlaskForm):
 	submit = SubmitField('Publish post')
 
 
-
 class UpdateAccount(FlaskForm):
-	
-	username = StringField('Username', validators = [
+	username = StringField('Username', validators=[
 		DataRequired(),
-		Length(min=5, max=20)		
+		Length(min=5, max=20)
 	])
 	email = StringField('Email', validators=[
 		DataRequired(),
-		Email()		
+		Email()
 	])
-	# password = PasswordField('Password', validators=[
-	# 	DataRequired(),
-	# 	Length(min=8)	
-	# ])
 	submit = SubmitField('Update my account.')
 
 	def validate_username(self, username):
@@ -105,10 +100,28 @@ class UpdateAccount(FlaskForm):
 		if email.data != current_user.email:
 			user = User.query.filter_by(email=email.data).first()
 			if user:
-				raise ValidationError('Email is already in use.')	
+				raise ValidationError('Email is already in use.')
 
-	# def validate_password(self, password):
-	# 	user = User.query.filter_by(email=password.data).first()
-	# 	if not bcrypt.check_password_hash(user.password, password.data):
-	# 		raise ValidationError('Wrong password.')	
 
+class PasswordReset(FlaskForm):
+	email = StringField('Email', validators=[
+		DataRequired(),
+		Email()
+	])
+	password = StringField('Password', validators=[
+		DataRequired()
+	])
+	submit = SubmitField('Change my password.')
+
+
+class ForgotPassword(FlaskForm):
+	email = StringField('Email', validators=[
+		DataRequired(),
+		Email()
+	])
+	submit = SubmitField('Reset my password.')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user:
+			raise ValidationError('Email is already in use.')
