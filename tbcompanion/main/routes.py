cc89c8acf72b00ed790a11508976ad66256e9418
GiorginoSerbuciano <64908978+GiorginoSerbuciano@users.noreply.github.com>
2021-03-01 12:16:40 +0100

    Public release

--- tbcompanion/main/routes.py
+++ tbcompanion/main/routes.py
@@ -1,14 +1,13 @@
-import os
-
 from flask import render_template, Blueprint
 from tbcompanion.models import Post, Project
+from sqlalchemy import desc
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-	posts = Post.query.all()
-	projects = Project.query.all()
+	posts = Post.query.order_by(desc('date')).all()
+	projects = Project.query.order_by(desc('date')).all()
 	return render_template('home.html', title='Home', posts=posts, projects=projects)
 
 @main.route('/about')
