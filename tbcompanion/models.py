fbf3da8170a4d5896e64e90b139022bcf8081b78
GiorginoSerbuciano <64908978+GiorginoSerbuciano@users.noreply.github.com>
2021-03-14 21:17:21 +0100

    User-Project association table

--- tbcompanion/models.py
+++ tbcompanion/models.py
@@ -6,7 +6,10 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 from tbcompanion import db, login_man
 
-
+contributor_table = db.Table('contributor_table',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('project_id', db.Integer, db.ForeignKey('project.id'))
+)
 
 @login_man.user_loader
 def load_user(user_id):
@@ -14,19 +17,21 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
+
+	post_author = db.relationship('Post', backref='author', lazy=True)
+	project_admin = db.relationship('Project', backref='admin', lazy=True)
+	project_contributor = db.relationship('Project', secondary=contributor_table, back_populates='contributor_id')
+		
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(120), unique=True, nullable=False)
 	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
 	password = db.Column(db.String(60), nullable=False)
 	is_admin = db.Column(db.Boolean)
-	post_author = db.relationship('Post', backref='author', lazy=True)
-	project_admin = db.relationship('Project', backref='admin', lazy=True)
-	project_contributor = db.relationship('Project', backref='contributor', lazy=True)
+
 
 	def __repr__(self):
-		return "User('{}','{}','{}')".format(
-			self.username, self.email, self.image_file, self.post_author)
+		return self.username
 
 	def get_reset_token(self, expires_sec=900):
 		s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
@@ -55,6 +60,9 @@ class Post(db.Model):
 
 
 class Project(db.Model):
+
+	contributor_id = db.relationship('User', secondary=contributor_table, back_populates='project_contributor')
+
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(120), nullable=False)
 	date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -65,5 +73,5 @@ class Project(db.Model):
 
 	def __repr__(self):
 		return "Project('{}','{}','{}')".format(
-			self.title, self.date)
+			self.title, self.date, self.content)
 
