cb516df994dc27de47faaae676848b8b0cb59418
GiorginoSerbuciano <64908978+GiorginoSerbuciano@users.noreply.github.com>
2021-04-04 16:57:22 +0200

    Revert "Revert "Giorgino serbuciano/issue8""

--- tbcompanion/projects/routes.py
+++ tbcompanion/projects/routes.py
@@ -4,7 +4,7 @@ from flask_login import current_user
 from flask_login.utils import login_required
 from tbcompanion import db
 from tbcompanion.projects.forms import ProjectForm
-from tbcompanion.models import Project
+from tbcompanion.models import Project, User
 from werkzeug.utils import redirect
 import markdown
 
@@ -20,16 +20,19 @@ def project(project_id):
 def project_form():
 	form = ProjectForm()
 	tags = form.dropdown_tags
-	print('pass')
 	if form.validate_on_submit():
-		print('validated')
 		project = Project(
 			title=form.title.data,
 			content=form.content.data,
-			contributor=current_user,
+			admin=current_user, 
+			#ISSUE: #10 AttributeError: 'str' object has no attribute '_sa_instance_state'
 			github_repo=form.github_repo.data,
 			tag=form.tag.data
 			)
+		contributors_list = form.contributors.data.split(', ')
+		for c in contributors_list:
+			contributor = User.query.filter_by(username=c).first()
+			project.contributor_id.append(contributor)
 		db.session.add(project)
 		db.session.commit()
 		flash('Your project is live!', 'success')
