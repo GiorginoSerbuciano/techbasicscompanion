1d2de203a20e328d7f4dbf867150661ed78bd4d2
GiorginoSerbuciano <64908978+GiorginoSerbuciano@users.noreply.github.com>
2021-04-04 15:28:33 +0200

    Revert "Revert "Giorgino serbuciano/issue8""

--- tbcompanion/projects/forms.py
+++ tbcompanion/projects/forms.py
@@ -3,7 +3,7 @@ from flask_pagedown.fields import PageDownField
 from wtforms.fields.core import SelectField, StringField
 from wtforms.fields.simple import SubmitField, TextAreaField
 from wtforms.validators import URL, DataRequired, Length, ValidationError
-from tbcompanion.models import Project
+from tbcompanion.models import Project, User
 from urllib.parse import urlparse
 
 
@@ -17,8 +17,8 @@ class ProjectForm(FlaskForm):
 	])
 	github_repo = StringField('URL to GitHub Repository', validators=[
 		Length(max=120),
-		URL()
 	])
+	contributors = StringField('List contributors in the format "User1, User2, User3"')
 	dropdown_tags = [
 		(None,'None'), 
 		('art','Art'), 
@@ -30,7 +30,7 @@ class ProjectForm(FlaskForm):
 	tag = SelectField('Tag',choices=dropdown_tags)
 	submit = SubmitField('Release project')
 	
-#TODO: Add contributors
+#TODO: #8 Add contributors
 
 	def validate_github_repo(self, github_repo):
 		project = Project.query.filter_by(github_repo=github_repo.data).first()
@@ -39,3 +39,13 @@ class ProjectForm(FlaskForm):
 			raise ValidationError('This GitHub repository is linked to a different project. Please update the already-existing project if you are a contributor.')
 		elif parse.scheme != 'https' or parse.netloc != 'github.com':
 			raise ValidationError('Invalid URL.')
+
+	def validate_contributors(self, contributors):
+		contributors_list = contributors.data.split(', ')
+		if len(contributors_list) > 1:
+			for contributor in contributors_list:
+				user = User.query.filter_by(username=contributor).first()
+				if not user:
+					raise ValidationError(f'The user "{contributor}" does not exist!')
+				elif user == current_user:
+					raise ValidationError(f'No need to pass yourself as a contributor ;).')
